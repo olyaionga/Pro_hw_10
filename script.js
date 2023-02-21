7e9e1438dc33d8bea7717836e1bbaf6136b4d79c
Olya Ionga <olechka2888@gmail.com>
2023-02-21 21:54:09 +0200

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+const length = parseInt(prompt("Введіть довжину масиву:"));
+const array = [];
+for (let i = 0; i < length; i++) {
+  array.push(prompt(`Введіть ${i + 1}-й елемент масиву:`));
+}
+
+document.write(`Початковий масив: [${array}]<br>`);
+
+array.sort();
+document.write(`Відсортований масив: [${array}]<br>`);
+
+array.splice(1, 4);
+document.write(`Масив після видалення елементів з 2 по 4: [${array}]<br>`);
