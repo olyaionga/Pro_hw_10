a287d4aaa68fe3ffd4d5bf24e5c953268e880ad4
Olya Ionga <olechka2888@gmail.com>
2023-02-22 18:47:39 +0200

    update

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ document.write(`Початковий масив: [${array}]<br>`);
 array.sort();
 document.write(`Відсортований масив: [${array}]<br>`);
 
-array.splice(1, 4);
+array.splice(1, 3);
 document.write(`Масив після видалення елементів з 2 по 4: [${array}]<br>`);
